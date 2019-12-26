2dca1b48dba45924fbcf82314715f8f051698e91
VVP <wolodin@vvp24.ru>
2019-12-26 12:45:47 +0500

    add input data from cmd

--- cmd.php
+++ cmd.php
@@ -1,7 +1,8 @@
 <?php
 
 require __DIR__ . '/function.php';
-$input = "аоарао Привет иван crhrivhrv rhvurvhuirfhv \n crhrivhrv  аоарао   crhrivhrv-crhrivhrv привет как дела
+
+$input  = $argv[1] ?? "аоарао Привет иван crhrivhrv rhvurvhuirfhv \n crhrivhrv  аоарао   crhrivhrv-crhrivhrv привет как дела
 работаешь Иван привет huhuh. rvtvvtgtg аоарао
 ";
 $output = getFivePopularWords($input);
